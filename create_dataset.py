1d17d92c1ad98790880c9a0a25c73f4d2b3ccab9
breadbread1984 <breadbread1984@163.com>
2021-09-27 10:24:22 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -265,22 +265,22 @@ class KittiDataset(object):
               # get radius
               # larger solution of x^2 - (height + width) x + (width * height * (1 - 0.7) / (1 + 0.7)) = 0
               a1 = 1;
-              b1 = height + width;
+              b1 = -(height + width);
               c1 = width * height * (1 - 0.7) / (1 + 0.7);
               sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1);
-              r1 = (b1 + sq1) / 2;
+              r1 = (-b1 + sq1) / (2 * a1); # larger solution is (-b + sqrt(b^2-4a*c)) / (2 * a)
               # larger solution of 4 x^2 - 2 * (height + width) x + (1 - 0.7) * width * height = 0
               a2 = 4;
-              b2 = 2 * (height + width);
+              b2 = -2 * (height + width);
               c2 = (1 - 0.7) * width * height;
               sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2);
-              r2 = (b2 + sq2) / 2;
+              r2 = (-b2 + sq2) / (2 * a2); # larger solution is (-b + sqrt(b^2-4a*c)) / (2 * a)
               # larger solution of 4 * 0.7 x^2 + 2 * 0.7 * (height + width) x + (0.7 - 1) * width * height = 0
               a3 = 4 * 0.7;
-              b3 = -2 * 0.7 * (height + width);
+              b3 = 2 * 0.7 * (height + width);
               c3 = (0.7 - 1) * width * height;
               sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3);
-              r3 = (b3 + sq3) / 2;
+              r3 = (-b3 + sq3) / (2 * a3); # larger solution is (-b + sqrt(b^2-4a*c)) / (2 * a)
               radius = max(0, int(min(r1, r2, r3)));
               # 
               center_y = (x - self.boundary['minX']) / 
